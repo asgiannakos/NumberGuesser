7ff87873c901fa3782d71457a4fabe438fb9a007
asgiannakos <asgiannakos@gmail.com>
2022-02-11 13:47:22 -0500

    First Commit

---
+++ NumberGuesser.js
@@ -0,0 +1,31 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+//generates a randome number that will be used as a target
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+};
+
+//finds the closer number between the user's and the computer's guesses to the target
+const compareGuesses = (userGuess, computerGuess, secretTarget) => {
+  let userDist = Math.abs(userGuess - secretTarget);
+  let computerDist = Math.abs(computerGuess - secretTarget);
+  if (computerDist < userDist) {
+    return false;
+  }
+  return true;
+};
+
+//updates the score of the winner
+const updateScore = winner => {
+  if (winner === 'computer') {
+    computerScore += 1;
+  }
+  humanScore +=1;
+};
+
+//updates the round number
+const advanceRound = () => {
+  currentRoundNumber += 1;
